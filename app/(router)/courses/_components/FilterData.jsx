9c516979de068655e63206de535d879e35a28e39
Ashwini <akamzing412@gmail.com>
2024-03-27 13:02:58 +0530

    fixed search bar

--- app/(router)/courses/_components/FilterData.jsx
+++ app/(router)/courses/_components/FilterData.jsx
@@ -3,7 +3,7 @@ import { Search } from 'lucide-react'
 
 const FilterData = () => {
     return (
-        <div className='flex justify-center p-2 rounded-md mb-4 bg-white'>
+        <div className='flex md:hidden justify-center p-2 rounded-md mb-4 bg-white'>
             <div className='flex md:gap-2 p-2 md:border md:rounded-md'>
                 <Search className='h-5 w-5' />
                 <input className='outline-none flex-grow pl-2 ' type="text" placeholder='Search...' />
